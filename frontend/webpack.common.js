c4a68e2c8514db178dcaa32c93018a1e9f590a46
Kyohei Seo <kyo199333@gmail.com>
2020-06-05 21:43:04 +0900

    modify

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -11,9 +11,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
     ],
   },
